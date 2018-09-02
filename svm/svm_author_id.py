6c2feadd5b131b99f8476145e27355aa3e84905b
rajdeepbharati <rajdeepbharati13@gmail.com>
2018-09-02 21:32:42 +0530

    Added SVM Email Classifier

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,36 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+clf = svm.SVC(kernel='rbf', C=10000)
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+pred = clf.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+
+count = 0
+for i in range(len(pred)):
+	if pred[i]==1:	# if email is sent by Chris
+		count+=1
+
+print count
+
+a1 = pred[10]
+a2 = pred[26]
+a3 = pred[50]
+
+print a1, " ", a2, " ", a3
+
+print "Accuracy: ", accuracy
+
+
 #########################################################
 
 
