3895f70f9c0bfedb52a2a88aa2db4c7e7836ea80
rajdeepbharati <rajdeepbharati13@gmail.com>
2018-09-02 22:59:14 +0530

    Added DecisionTreeClassifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+print "Number of features: ", len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+
+print "Accuracy: ", accuracy
 
 #########################################################
 
