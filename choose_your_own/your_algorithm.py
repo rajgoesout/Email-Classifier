6d04c44383deadaf03d1feb887a3385c2ed70d48
rajdeepbharati <rajdeepbharati13@gmail.com>
2018-09-04 19:13:32 +0530

    Added KNN Terrain Classifier

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,12 +31,15 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(n_neighbors=1)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
 
-
-
-
-
+print "Accuracy:", accuracy
 
 try:
     prettyPicture(clf, features_test, labels_test)
